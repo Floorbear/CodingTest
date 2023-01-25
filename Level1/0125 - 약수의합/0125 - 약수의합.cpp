da6a7375632271ecd42284a11edba29ab459c584
floorbear2723 <floorbear2723@gmail.com>
2023-01-25 16:47:08 +0900

    이동원 : 코테 공부중

---
+++ Level1/0125 - 약수의합/0125 - 약수의합.cpp
@@ -0,0 +1,21 @@
+﻿#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(int n) {
+	int answer = 0;
+	for (int i = 0; i < n; i++)
+	{
+		int DivValue = i + 1;
+		if (n % DivValue == 0)
+		{
+			answer += DivValue;
+		}
+	}
+	return answer;
+}
+int main()
+{
+	solution(12);
+}
---
+++ Level1/0125 - 평균 구하기/0125 - 평균 구하기.cpp
@@ -0,0 +1,22 @@
+﻿#include <string>
+#include <vector>
+
+using namespace std;
+
+double solution(vector<int> arr) {
+	double answer = 0;
+	int Count = 0;
+	int Add = 0;
+	for (int i = 0; i < arr.size(); i++)
+	{
+		Count++;
+		Add += arr[i];
+	}
+
+	answer = static_cast<double>(Add) / static_cast<double>(Count);
+	return answer;
+}
+int main()
+{
+	solution({ 1,2,3,4 });
+}
