1f0dc8516b0db42ff8e31652d6a3c58a6ed8fa23
Floorbear <floorbear2723@gmail.com>
2023-03-06 15:06:23 +0900

    이동원 : 0306
    클

---
+++ Level1/0306 - 삼총사/0306 - 삼총사.cpp
@@ -0,0 +1,37 @@
+﻿#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(vector<int> number) {
+    int result = 0;
+    for (int i = 0; i < number.size() - 2; i++)
+    {
+        int first = -1;
+        int second = -1;
+        int third = -1;
+
+        first = number[i];
+        for (int j = i + 1; j < number.size() - 1; j++)
+        {
+            second = number[j];
+            for (int k = j + 1; k < number.size(); k++)
+            {
+                third = number[k];
+                int sum = first + second + third;
+                if (sum == 0)
+                {
+                    result++;
+                }
+            }
+        }
+    }
+
+    return result;
+}
+
+int main()
+{
+    solution({ -2, 3, 0, 2, -5 });//return 2
+}
+
