eff9dff9a989fee4e604921348e88d12a4064b56
Floorbear <floorbear2723@gmail.com>
2023-10-23 17:06:42 +0900

    이동원 : 연속된 부분 수열의 합 구현

---
+++ 0921 - 요격 시스템/0921 - 요격 시스템.cpp
@@ -0,0 +1,149 @@
+﻿
+#include <iostream>
+#include <vector>
+#include <string>
+#include <algorithm>
+#include <list>
+
+struct Pos2
+{
+    int x = 0;
+    int y = 0;
+
+    bool IsInRange(int _Value)
+    {
+        if (_Value > x && _Value < y)
+            return true;
+
+        return false;
+    }
+
+
+    bool IsInRange(Pos2 _Value)
+    {
+        if (_Value.x > x  && _Value.x < y )
+            return true;
+
+        if (_Value.y > x && _Value.y < y)
+            return true;
+
+        if (_Value.x == x && _Value.y == y)
+            return true;
+
+        return false;
+    }
+
+    bool IsInRange(float _x,float _y)
+    {
+        if (_x > x && _x < y)
+            return true;
+
+        if (_y > x && _y < y)
+            return true;
+
+        if (_x == x && _y == y)
+            return true;
+
+        return false;
+    }
+
+    int GetLength()
+    {
+        return y - x;
+    }
+
+    bool operator==(const Pos2& _Compare)
+    {
+        if (_Compare.x == this->x && _Compare.y == this->y)
+            return true;
+
+        return false;
+    }
+
+    float GetCenter()
+    {
+        return (float)(x + y) / 2.f;
+    }
+
+};
+
+Pos2 FindShortestMissile(const std::list<Pos2>& _Missiles)
+{
+    Pos2 ShortestMissile;
+    for (auto Missile : _Missiles)
+    {
+        if(ShortestMissile.GetLength() == 0)
+            ShortestMissile = Missile;
+
+        if (Missile.GetLength() < ShortestMissile.GetLength())
+        {
+            ShortestMissile = Missile;
+        }
+    }
+
+    return ShortestMissile;
+}
+
+std::list<Pos2> FindOverlappedMissiles(Pos2 _Scope,std::list<Pos2>& _Missiles)
+{
+    std::list<Pos2> LeftOverlapped;
+    std::list<Pos2> RightOverlapped;
+    for (auto Missile : _Missiles)
+    {
+        if (Missile.IsInRange(_Scope.x, _Scope.GetCenter()))
+        {
+            LeftOverlapped.push_back(Missile);
+        }
+
+        if (Missile.IsInRange(_Scope.GetCenter(), _Scope.y))
+        {
+            RightOverlapped.push_back(Missile);
+        }
+    }
+
+
+    if (LeftOverlapped.size() >= RightOverlapped.size())
+    {
+        return LeftOverlapped;
+    }
+    else
+    {
+        return RightOverlapped;
+    }
+}
+
+int solution(std::vector<std::vector<int>> targets) {
+    int answer = 0;
+
+    //미사일들 저장
+    std::list<Pos2> Missiles;
+    for (auto target : targets)
+    {
+        Pos2 NewMissle;
+        NewMissle.x = target[0];
+        NewMissle.y = target[1];
+        Missiles.push_back(NewMissle);
+    }
+
+    while (Missiles.empty() != true)
+    {
+        std::list<Pos2> ShouldRemoveMissiles = FindOverlappedMissiles(FindShortestMissile(Missiles), Missiles);
+        for (std::list<Pos2>::iterator i = ShouldRemoveMissiles.begin(); i != ShouldRemoveMissiles.end(); ++i)
+        {
+            Missiles.remove_if([&](Pos2& _Other) { return _Other == *i; });
+        }
+        answer++;
+    }
+
+
+    
+
+
+    return answer;
+}
+
+int main()
+{
+    //solution([[4,5],[4,8],[10,14],[11,13],[5,12],[3,7],[1,4]])
+    solution({ {4,5},{4,8},{10,14},{11,13},{5,12},{3,7},{1,4} }); //result = 3
+}
---
+++ Level2/1023 - 연속된 부분 수열의 합/1023 - 연속된 부분 수열의 합.cpp
@@ -0,0 +1,116 @@
+﻿#include <string>
+#include <vector>
+
+using namespace std;
+
+struct Sequence 
+{
+	int StartIndex = 0;
+	int EndIndex = 0;
+	int GetLength()
+	{
+		return EndIndex - StartIndex;
+	}
+};
+
+// -1 : not match
+bool IsExistSubsequence(int _K, int _StartIndex,int* _EndIndex, vector<int> _Sequence,int* _SumValue)
+{
+	int SequenceLast = _Sequence.size() - 1;
+	while (*_SumValue < _K && *_EndIndex <= SequenceLast)
+	{
+		*_EndIndex += 1;
+		*_SumValue += _Sequence[*_EndIndex];
+		
+	}
+	//*_EndIndex -= 1;
+	if (*_SumValue == _K)
+	{
+		return true;
+	}
+	
+	return false;
+}
+
+vector<int> solution(vector<int> sequence, int k) {
+	vector<int> answer;
+
+	vector<Sequence> Candidates_1;
+	vector<Sequence> Candidates_2;
+	int MostShortLength = -1;
+
+	int SumValue = sequence[0];
+
+	int EndIndex = 0;
+	int SquenceEndIndex = sequence.size() - 1;
+	bool bIsSubsequence = false;
+	for (int i = 0; i < sequence.size(); ++i)
+	{
+		while (SumValue < k && EndIndex <= SquenceEndIndex)
+		{
+			EndIndex += 1;
+			SumValue += sequence[EndIndex];
+
+		}
+		//*_EndIndex -= 1;
+		if (SumValue == k)
+		{
+			bIsSubsequence = true;
+		}
+		else
+		{
+			bIsSubsequence = false;
+
+		}
+		if (bIsSubsequence == true)
+		{
+			Sequence NewSequence;
+			NewSequence.StartIndex = i;
+			NewSequence.EndIndex = EndIndex;
+			if (MostShortLength == -1)
+			{
+				MostShortLength = NewSequence.GetLength();
+			}
+			if (NewSequence.GetLength() < MostShortLength)
+			{
+				MostShortLength = NewSequence.GetLength();
+			}
+			Candidates_1.push_back(NewSequence);
+		}
+		SumValue -= sequence[i];
+	}
+
+	if (Candidates_1.size() == 1)
+	{
+		answer.push_back(Candidates_1[0].StartIndex);
+		answer.push_back(Candidates_1[0].EndIndex);
+		return answer;
+	}
+
+	for (int i = 0; i < Candidates_1.size(); ++i)
+	{
+		if (Candidates_1[i].GetLength() != MostShortLength)
+		{
+			continue;
+		}
+
+		Candidates_2.push_back(Candidates_1[i]);
+	}
+
+	if (Candidates_2.empty() == true)
+	{
+		answer.push_back(Candidates_1[0].StartIndex);
+		answer.push_back(Candidates_1[0].EndIndex);
+		return answer;
+	}
+	answer.push_back(Candidates_2[0].StartIndex);
+	answer.push_back(Candidates_2[0].EndIndex);
+	return answer;
+}
+
+int main()
+{
+	solution({ 1, 1, 1, 2, 3, 4, 5 }, 5);
+}
+
+
